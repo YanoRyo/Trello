760ac871a4862741c6866de307fdb0e9a7741c2e
YanoRyo <yanoryo7032@gmail.com>
2022-08-20 11:55:48 +0900

    Intro画面作成

--- app/src/main/java/com/example/trello/SplashActivity.kt
+++ app/src/main/java/com/example/trello/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.trello
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
 import android.widget.TextView
 
@@ -20,5 +22,10 @@ class SplashActivity : AppCompatActivity() {
         )
         val typeFace: Typeface = Typeface.createFromAsset(assets,"carbon bl.ttf")
         appName.typeface = typeFace
+
+        Handler().postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+        },2500)
     }
 }
