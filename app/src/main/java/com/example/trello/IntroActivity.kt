86629ff16d87d60a7cecf158895736acdd0e6aa9
YanoRyo <yanoryo7032@gmail.com>
2022-08-20 15:02:19 +0900

    SignUp画面の作成

--- app/src/main/java/com/example/trello/IntroActivity.kt
+++ app/src/main/java/com/example/trello/IntroActivity.kt
@@ -1,17 +1,29 @@
 package com.example.trello
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        val btnSignUP: Button = findViewById(R.id.btn_sign_up_intro)
+        val btnSignIn: Button = findViewById(R.id.btn_sign_in_intro)
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        btnSignUP.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+
+        }
+
     }
 }
