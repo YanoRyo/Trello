cea5418acfd7e0f526d8b9465c08deb80a29fff4
矢野涼 <yanoryo@yanoryounoMacBook-Air.local>
2023-02-21 00:05:25 +0900

    ハンバーガーメニューの機能

--- app/src/main/java/com/example/trello/activities/MainActivity.kt
+++ app/src/main/java/com/example/trello/activities/MainActivity.kt
@@ -1,17 +1,75 @@
 package com.example.trello.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.trello.R
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBar()
+        val navView = this.findViewById<NavigationView>(R.id.nav_view)
+        navView.setNavigationItemSelectedListener(this)
+
         // 「android.R.color.transparent」を指定することで、ステータスバーを透過させている
         window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
     }
+
+    private fun setupActionBar() {
+        val toolbar = this.findViewById<Toolbar>(R.id.toolbar_main_activity)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolbar.setNavigationOnClickListener {
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer() {
+        val drawLayout = findViewById<DrawerLayout>(R.id.draw_layout)
+        if (drawLayout.isDrawerOpen(GravityCompat.START)) {
+            drawLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed() {
+        val drawLayout = findViewById<DrawerLayout>(R.id.draw_layout)
+        if (drawLayout.isDrawerOpen(GravityCompat.START)) {
+            drawLayout.closeDrawer(GravityCompat.START)
+        } else {
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val drawLayout = findViewById<DrawerLayout>(R.id.draw_layout)
+        when (item.itemId) {
+            R.id.nav_my_profile -> {
+                Toast.makeText(this@MainActivity, "My Profile", Toast.LENGTH_LONG)
+            }
+            R.id.nav_sign_out -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
 }
