14ec7ab6dfa646187aaad708e2033305cc5e6c68
YanoRyo <yanoryo7032@gmail.com>
2023-02-25 16:20:39 +0900

    プロフィール画像をFirebaseのStorageに保存

--- app/src/main/java/com/example/trello/activities/MyProfileActivity.kt
+++ app/src/main/java/com/example/trello/activities/MyProfileActivity.kt
@@ -1,22 +1,113 @@
 package com.example.trello.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.webkit.MimeTypeMap
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.trello.R
 import com.example.trello.firebase.FireStoreClass
 import com.example.trello.models.User
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var ivUserImage: CircleImageView? = null
+    private var mSelectedImageFileUri: Uri? = null
+    private var mProfileImageURL: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
+        ivUserImage = findViewById(R.id.iv_profile_user_image)
+        val btnUpdate = findViewById<Button>(R.id.btn_update)
 
         setupActionBar()
 
         FireStoreClass().loadUserData(this)
+
+        ivUserImage?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE)
+            }
+        }
+        btnUpdate.setOnClickListener {
+            if (mSelectedImageFileUri != null) {
+                uploadUserImage()
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+            }
+        } else {
+            Toast.makeText(
+                this, "Oops you just denied the permission for storage.",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showImageChooser() {
+        var garalleryIntent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(garalleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        ivUserImage = findViewById(R.id.iv_profile_user_image)
+        if (data != null) {
+            if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data.data != null) {
+                mSelectedImageFileUri = data.data
+
+                try {
+                    ivUserImage?.let {
+                        Glide
+                            .with(this)
+                            .load(mSelectedImageFileUri)
+                            .centerCrop()
+                            .placeholder(R.drawable.ic_user_place_holder)
+                            .into(it)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
 
     private fun setupActionBar() {
@@ -36,22 +127,58 @@ class MyProfileActivity : BaseActivity() {
     }
 
     fun setUserDataInUI(user: User) {
-        val ivUserImage = findViewById<CircleImageView>(R.id.iv_user_image)
+        ivUserImage = findViewById(R.id.iv_profile_user_image)
         val etUserName = findViewById<TextView>(R.id.et_name)
         val etUserEmail = findViewById<TextView>(R.id.et_email)
         val etUserNumber = findViewById<TextView>(R.id.et_mobile)
-        Glide
-            .with(this)
-            .load(user.image)
-            .centerCrop()
-            .placeholder(R.drawable.ic_user_place_holder)
-            .into(ivUserImage)
+        ivUserImage?.let {
+            Glide
+                .with(this)
+                .load(user.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_user_place_holder)
+                .into(it)
+        }
         etUserName.text = user.name
         etUserEmail.text = user.email
         if (user.mobile != null) {
             etUserNumber.text = user.mobile.toString()
         }
+    }
 
+    private fun uploadUserImage() {
+        val btnUpdate = findViewById<Button>(R.id.btn_update)
+        showProgressDialog(resources.getString(R.string.please_wait))
+
+        if (mSelectedImageFileUri != null) {
+            val sRef: StorageReference =
+                FirebaseStorage.getInstance().reference.child(
+                    "USER_IMAGE" + System.currentTimeMillis() + "." + getFileExtension(
+                        mSelectedImageFileUri))
+            sRef.putFile(mSelectedImageFileUri!!).addOnSuccessListener { taskSnapshop ->
+                Log.i(
+                    "Firebase Image URL",
+                    taskSnapshop!!.metadata!!.reference!!.downloadUrl.toString())
+                taskSnapshop.metadata!!.reference!!.downloadUrl.addOnSuccessListener { uri ->
+                    Log.i("Downloadable Image URL", uri.toString())
+                    mProfileImageURL = uri.toString()
+
+                    // TODO updateUserProfileData
+                }
+            }.addOnFailureListener { exception ->
+                Toast.makeText(this, exception.message, Toast.LENGTH_LONG).show()
+                hideProgressDialog()
+            }
+        }
+        btnUpdate.setOnClickListener {
+            if (mSelectedImageFileUri != null) {
+                uploadUserImage()
+            }
+        }
+    }
+
+    private fun getFileExtension(uri: Uri?): String? {
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(contentResolver.getType(uri!!))
     }
 
 }
